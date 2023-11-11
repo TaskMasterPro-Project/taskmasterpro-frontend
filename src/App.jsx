eb2b79fb47434f3e869a54742f67e47ac1716e36
Kaspi-Chan <bozhilov53@gmail.com>
2023-11-11 23:59:53 +0200

    Initial commit with Vite React setup and React Rounter installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Hi mom!</h1>
+    </>
+  )
+}
+
+export default App
