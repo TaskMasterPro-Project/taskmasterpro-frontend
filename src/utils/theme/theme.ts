15f6c61d333a26d51ec1a200ce007c472741ec29
Kaspi-Chan <bozhilov53@gmail.com>
2023-11-28 16:31:00 +0200

    Add color palette for both light and dark modes

--- src/utils/theme/theme.ts
+++ src/utils/theme/theme.ts
@@ -1,18 +1,74 @@
 import { purple, blue, green, yellow, grey } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#FFFFFF",
-        },
-        secondary: {
-            main: "#FF4500",
-        },
-        background: {
-            default: "#DAE0E6",
-        },
+const primary = {
+    primary50: '#E0FCFF',
+    primary100: '#BEF8FD',
+    primary200: '#87EAF2',
+    primary300: '#54D1DB',
+    primary400: '#38BEC9',
+    primary500: '#2CB1BC',
+    primary600: '#14919B',
+    primary700: '#0E7C86',
+    primary800: '#0A6C74',
+    primary900: '#044E54'
+  };
+  
+  const secondary = {
+    secondary50: '#F0F4F8',
+    secondary100: '#D9E2EC',
+    secondary200: '#BCCCDC',
+    secondary300: '#9FB3C8',
+    secondary400: '#829AB1',
+    secondary500: '#627D98',
+    secondary600: '#486581',
+    secondary700: '#334E68',
+    secondary800: '#243B53',
+    secondary900: '#102A43'
+};
+  
+declare module '@mui/material/styles' {
+    interface PaletteOptions {
+      primaryPalette?: typeof primary;
+      secondaryPalette?: typeof secondary;
     }
+  }
+
+type ThemeMode = 'light' | 'dark';
+
+const getTheme = (mode: ThemeMode) => createTheme({
+  palette: {
+    mode, // 'light' or 'dark'
+    primary: {
+      main: primary.primary600,
+      light: primary.primary400,
+      dark: primary.primary800,
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: mode === 'dark' ? secondary.secondary800 : secondary.secondary200,
+      light: mode === 'dark' ? secondary.secondary600 : secondary.secondary100,
+      dark: secondary.secondary900,
+    },
+    background: {
+      default: mode === 'dark' ? secondary.secondary900 : secondary.secondary100,
+      paper: mode === 'dark' ? secondary.secondary800 : '#fff'
+    },
+    error: {
+      main: '#BA2525',
+      light: '#FACDCD',
+      dark: '#A61B1B'
+    },
+    // adding all the palettes if needed in single cases
+    primaryPalette: primary,
+    secondaryPalette: secondary
+  },
 });
 
-export default theme;
+  // Create a light theme
+const lightTheme = getTheme('light');
+
+// Create a dark theme
+const darkTheme = getTheme('dark');
+
+export { lightTheme, darkTheme };
